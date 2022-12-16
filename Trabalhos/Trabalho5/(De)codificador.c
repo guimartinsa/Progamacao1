71a73de100c42094be4cb02dcb3b41163d39cf27
Your Name <guilhermemaretinsyoutub13@gmail.com>
2022-12-16 20:51:13 -0300

    (De)codificador
    Versão final

--- Trabalhos/Trabalho5/(De)codificador.c
+++ Trabalhos/Trabalho5/(De)codificador.c
@@ -11,116 +11,42 @@ deverá imprimir na tela mensagem M correspondente.
 A mensagem M deverá sempre ser tratada, tanto para leitura quanto para impressão, como uma sequência
 (vetor) de caracteres. Já a mensagem codificada C deverá sempre ser tratada, tanto para leitura quanto
 para impressão, como uma sequência (vetor) de números inteiros.
-A chave a ser utilizada em ambos os processos será K = "EARTE2020/1".
-*/
-
+A chave a ser utilizada em ambos os processos será K = "EARTE2020/1".*/
 #include <stdio.h>
 #include <locale.h>
 
 int main(){
     setlocale(LC_ALL,"Portuguese");
-    int i , j , cond=0 , tam=0;
-    char frase[100] , chave[100] , codificada[100] , decodificada[100];
-    printf("Oque deseja fazer?\n[1]Codificar\n[2]Decodificar\n[3]-Encerrar\n");
-    scanf("%d",&cond);
-    if(cond==1){
-        printf("Digite a frase que deseja codificar:");
-        scanf("%s",frase);
-        printf("Digite a chave:");
-        scanf("%s",chave);
-        for(i=0;frase[i]!='\0';i++){
-            codificada[i]=frase[i]+chave[i];
-        }
-        codificada[i]='\0';
-        printf("Frase codificada: %s \n",codificada);
-    }else if(cond==2){
-        printf("Digite o tamanho da frase que deseja decodificar:");
-        scanf("%d",&tam);
-        printf("Digite a frase que deseja decodificar:");
-        scanf("%s",frase);
-        printf("Digite a chave para descodificar a mensagem:");
-        scanf("%s",chave);
-        for(j=0;frase[j]!='\0';j++){
-            decodificada[j]=frase[j]-chave[j];
-        }
-        decodificada[j]='\0';
-        printf("Frase decodificada: %s \n",decodificada);
-    }else if(cond==3){
-        printf("Encerrando programa...");
-    }
-}
-
-
-
-
-
-
-
-
-
-/*int main (){
-    setlocale(LC_ALL,"Portuguese");
-    int i , j , cond=0;
-    char frase[100] , chave[100] , codificada[100] , decodificada[100];
-    printf("Oque deseja fazer?\n[1]Codificar\n[2]Decodificar\n[3]-Encerrar\n");
-    scanf("%d",&cond);
-    if(cond==1){
-        printf("Digite a frase que deseja codificar:");
-        scanf("%s",frase);
-        printf("Digite a chave:");
-        scanf("%s",chave);
-        for(i=0;frase[i]!='\0';i++){
-            codificada[i]=frase[i]+chave[i];
-        }
-        codificada[i]='\0';
-        printf("Frase codificada: %s \n",codificada);
-    }else if(cond==2){
-        printf("Digite a frase que deseja decodificar:");
-        scanf("%s",frase);
-        printf("Digite a chave para descodificar a mensagem:");
-        scanf("%s",chave);
-        for(j=0;frase[j]!='\0';j++){
-            decodificada[j]=frase[j]-chave[j];
-        }
-        decodificada[j]='\0';
-        printf("Frase decodificada: %s \n",decodificada);
-    }else if(cond==3){
-        printf("Encerrando programa...");
-    }
-
-
-
-    return 0;
-}
-
-    
-    /*printf("Digite a frase que deseja codificar: ");
-    gets(frase);
-    printf("Digite a chave: ");
-    gets(chave);
-    for(i=0;frase[i]!='\0';i++){
-        codificada[i]=frase[i]+chave[i];
-    }
-    codificada[i]='\0';
-    printf("Frase codificada: %s \n",codificada);
-    printf("Deseja decodificar a frase? (1-Sim/2-Não)");
-    gets(cond);
-    if(cond==1){
-        for(j=0;codificada[j]!='\0';j++){
-            decodificada[j]=codificada[j]-chave[j];
-        }
-        decodificada[j]='\0';
-        printf("Frase decodificada: %s \n",decodificada);
-    }else if(cond==2){
-        printf("Deseja codificar a frase? (1-Sim/2-Não)");
-        gets(cond);
+    int i , j , cond=0 , tam=0 , cont=0 , codificada[60] ;
+    char frase[60] , chave[60] , decodificada[60];
+    do{
+        printf("Oque deseja fazer?\n[1]Codificar\n[2]Decodificar\n[3]Encerrar\n");
+        scanf("%d",&cond);
         if(cond==1){
-            for(i=0;frase[i]!='\0';i++){
-                codificada[i]=frase[i]+chave[i];
+            printf("Digite a frase que deseja codificar:");
+            scanf("%s",frase);
+            printf("Digite a chave:");
+            scanf("%s",chave);
+            
+            for(i=0 ; frase[i]!='\0' ; i++){
+                codificada[i] = frase[i] ^ chave[i];
             }
             codificada[i]='\0';
-            printf("Frase codificada: %s \n",codificada);
+            printf("Frase codificada foi: %i \n", codificada);
+            printf("Tamanho da frase foi: %d \n", i);
+        }else if(cond==2){
+            printf("Digite o tamanho da frase que deseja decodificar:");
+            scanf("%d", &tam);
+            printf("Digite a chave para descodificar a mensagem:");
+            scanf("%s", chave);
+            for(j=0 ; frase[j]!='\0' ; j++){
+                decodificada[j] = codificada[j] ^ chave[j];
+            }
+            decodificada[j]='\0';
+            printf("Frase decodificada: %s \n", decodificada);
+        }else if(cond==3){
+            printf("Encerrando programa...");
         }
-    }
+    }while(cond!=3);
     return 0;
-}8*/
+}
